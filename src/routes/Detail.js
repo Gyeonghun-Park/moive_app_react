99cc7ce6ff0b5574a82202016944a5458d6bbf0f
Gyeonghun Park <gp112795@gmail.com>
2020-09-11 13:22:59 -0400

    Sharing Props Between Routes

---
+++ src/routes/Detail.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Detail.css";
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return (
+        <div className="detail__container">
+          <span className="detail__title">{location.state.title}</span>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
+
+export default Detail;
