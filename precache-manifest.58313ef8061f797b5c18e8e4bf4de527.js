ac8180741f18fce4d0706f55d050726814fda749
Gyeonghun Park <gp112795@gmail.com>
2020-09-11 12:28:10 -0400

    Updates

---
+++ precache-manifest.58313ef8061f797b5c18e8e4bf4de527.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37b436e9d0737dc418ab92a9558f0d2",
+    "url": "/Gyeonghun-Park/moive_app_react/index.html"
+  },
+  {
+    "revision": "54849e9062ad50f5c88d",
+    "url": "/Gyeonghun-Park/moive_app_react/static/css/main.f22166bb.chunk.css"
+  },
+  {
+    "revision": "2d5a4374c36e1f55908d",
+    "url": "/Gyeonghun-Park/moive_app_react/static/js/2.fcf50594.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gyeonghun-Park/moive_app_react/static/js/2.fcf50594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54849e9062ad50f5c88d",
+    "url": "/Gyeonghun-Park/moive_app_react/static/js/main.382a7b2f.chunk.js"
+  },
+  {
+    "revision": "fcbf7cb1841604d140a5",
+    "url": "/Gyeonghun-Park/moive_app_react/static/js/runtime-main.90b69600.js"
+  }
+]);
